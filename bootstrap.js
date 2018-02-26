5cd7183d261e84672fc162d08250578fc6d1a318
Mathieu Schroeter <mathieu@schroetersa.ch>
2018-02-26 22:56:34 +0100

    Adapt for latest prettier

--- bootstrap.js
+++ bootstrap.js
@@ -1,8 +1,8 @@
 'use strict';
 
-var path = require ('path');
+var path = require('path');
 
-var xPlatform = require ('xcraft-core-platform');
+var xPlatform = require('xcraft-core-platform');
 
 var cmd = {};
 
@@ -12,24 +12,24 @@ var cmd = {};
  * 1. Build and install CMake.
  * 2. Build and install WPKG.
  */
-cmd.wpkg = function* (msg, response, next) {
+cmd.wpkg = function*(msg, response, next) {
   try {
-    yield response.command.send ('cmake.build', null, next);
-    yield response.command.send ('wpkg.build', null, next);
+    yield response.command.send('cmake.build', null, next);
+    yield response.command.send('wpkg.build', null, next);
 
-    response.log.info ('wpkg bootstrapped');
+    response.log.info('wpkg bootstrapped');
   } catch (ex) {
-    response.log.err (ex.stack || ex);
+    response.log.err(ex.stack || ex);
   } finally {
-    response.events.send (`bootstrap.wpkg.${msg.id}.finished`);
+    response.events.send(`bootstrap.wpkg.${msg.id}.finished`);
   }
 };
 
 /**
  * Bootstrap the peon.
  */
-cmd.peon = function* (msg, response, next) {
-  const boot = 'bootstrap+' + xPlatform.getOs ();
+cmd.peon = function*(msg, response, next) {
+  const boot = 'bootstrap+' + xPlatform.getOs();
 
   let cmdMsg = null;
   let result = null;
@@ -40,7 +40,7 @@ cmd.peon = function* (msg, response, next) {
       packageArgs: [boot + ',@deps'],
     };
 
-    result = yield response.command.send ('pacman.make', cmdMsg, next);
+    result = yield response.command.send('pacman.make', cmdMsg, next);
     if (result.data === response.events.status.failed) {
       throw 'the command has failed';
     }
@@ -50,22 +50,22 @@ cmd.peon = function* (msg, response, next) {
     };
 
     /* Build bootstrap packages. */
-    result = yield response.command.send ('pacman.build', cmdMsg, next);
+    result = yield response.command.send('pacman.build', cmdMsg, next);
     if (result.data === response.events.status.failed) {
       throw 'the command has failed';
     }
 
     /* Install bootstrap package. */
-    result = yield response.command.send ('pacman.install', cmdMsg, next);
+    result = yield response.command.send('pacman.install', cmdMsg, next);
     if (result.data === response.events.status.failed) {
       throw 'the command has failed';
     }
 
-    response.log.info ('peon bootstrapped');
+    response.log.info('peon bootstrapped');
   } catch (ex) {
-    response.log.err (ex.stack || ex);
+    response.log.err(ex.stack || ex);
   } finally {
-    response.events.send (`bootstrap.peon.${msg.id}.finished`);
+    response.events.send(`bootstrap.peon.${msg.id}.finished`);
   }
 };
 
@@ -75,16 +75,16 @@ cmd.peon = function* (msg, response, next) {
  * 1. Bootstrap WPKG.
  * 2. Bootstrap the peon.
  */
-cmd.all = function* (msg, response, next) {
+cmd.all = function*(msg, response, next) {
   try {
-    yield response.command.send ('bootstrap.wpkg', null, next);
-    yield response.command.send ('bootstrap.peon', null, next);
+    yield response.command.send('bootstrap.wpkg', null, next);
+    yield response.command.send('bootstrap.peon', null, next);
 
-    response.log.info ('everything bootstrapped');
+    response.log.info('everything bootstrapped');
   } catch (ex) {
-    response.log.err (ex.stack || ex);
+    response.log.err(ex.stack || ex);
   } finally {
-    response.events.send (`bootstrap.all.${msg.id}.finished`);
+    response.events.send(`bootstrap.all.${msg.id}.finished`);
   }
 };
 
@@ -93,7 +93,7 @@ cmd.all = function* (msg, response, next) {
  *
  * @returns {Object} The list and definitions of commands.
  */
-exports.xcraftCommands = function () {
+exports.xcraftCommands = function() {
   return {
     handlers: cmd,
     rc: {
