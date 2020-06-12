10d8ed6ac61d4d8e8e38ec8f5b45cab5180fb516
Mathieu Schroeter <mathieu@schroetersa.ch>
2020-06-12 22:35:11 +0200

    Remove obsolete bootstrap.peon stuff

--- bootstrap.js
+++ bootstrap.js
@@ -1,9 +1,5 @@
 'use strict';
 
-var path = require('path');
-
-var xPlatform = require('xcraft-core-platform');
-
 var cmd = {};
 
 /**
@@ -26,69 +22,6 @@ cmd.wpkg = function* (msg, resp, next) {
 };
 
 /**
- * Bootstrap the peon.
- */
-cmd.peon = function* (msg, resp, next) {
-  const boot = 'bootstrap+' + xPlatform.getOs();
-
-  let cmdMsg = null;
-  let result = null;
-
-  try {
-    /* Make bootstrap packages and all deps. */
-    cmdMsg = {
-      packageArgs: [boot + ',@deps'],
-    };
-
-    result = yield resp.command.send('pacman.make', cmdMsg, next);
-    if (result.data === resp.events.status.failed) {
-      throw 'the command has failed';
-    }
-
-    cmdMsg = {
-      packageRefs: boot,
-    };
-
-    /* Build bootstrap packages. */
-    result = yield resp.command.send('pacman.build', cmdMsg, next);
-    if (result.data === resp.events.status.failed) {
-      throw 'the command has failed';
-    }
-
-    /* Install bootstrap package. */
-    result = yield resp.command.send('pacman.install', cmdMsg, next);
-    if (result.data === resp.events.status.failed) {
-      throw 'the command has failed';
-    }
-
-    resp.log.info('peon bootstrapped');
-  } catch (ex) {
-    resp.log.err(ex.stack || ex);
-  } finally {
-    resp.events.send(`bootstrap.peon.${msg.id}.finished`);
-  }
-};
-
-/**
- * Bootstrap everything.
- *
- * 1. Bootstrap WPKG.
- * 2. Bootstrap the peon.
- */
-cmd.all = function* (msg, resp, next) {
-  try {
-    yield resp.command.send('bootstrap.wpkg', null, next);
-    yield resp.command.send('bootstrap.peon', null, next);
-
-    resp.log.info('everything bootstrapped');
-  } catch (ex) {
-    resp.log.err(ex.stack || ex);
-  } finally {
-    resp.events.send(`bootstrap.all.${msg.id}.finished`);
-  }
-};
-
-/**
  * Retrieve the list of available commands.
  *
  * @returns {Object} The list and definitions of commands.
@@ -99,12 +32,6 @@ exports.xcraftCommands = function () {
     rc: {
       wpkg: {
         desc: 'bootstrap the package manager',
-      },
-      peon: {
-        desc: 'bootstrap for devroot/',
-      },
-      all: {
-        desc: 'bootstrap everything',
       },
     },
   };
